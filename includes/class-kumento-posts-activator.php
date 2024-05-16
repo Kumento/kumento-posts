bb5566dccc3e9ca0b0f347db9168978a14b59ba1
Ulrik Vander <ulrik@vander.dk>
2024-05-16 12:59:51 +0200

    Initial upload

---
+++ includes/class-kumento-posts-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://kumento.com/
+ * @since      1.0.0
+ *
+ * @package    Kumento_Posts
+ * @subpackage Kumento_Posts/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Kumento_Posts
+ * @subpackage Kumento_Posts/includes
+ * @author     Ulrik Vander <ulrik@vander.dk>
+ */
+class Kumento_Posts_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
