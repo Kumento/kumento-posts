b6dbe01271f214c734c955459e65012a68d25f14
Ulrik Vander <ulrik@vander-web.com>
2024-05-29 23:27:17 +0200

    main

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  prefix: 'ku-',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
